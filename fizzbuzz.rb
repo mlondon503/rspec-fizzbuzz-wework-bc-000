e72ade0efcf2d212af5937ff93a37ed2368106f7
Michael London <michael.london@wework.com>
2018-02-10 21:16:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 def fizzbuzz(int)
-  if (int % 3 == 0 && int % 5 == 0)
-    puts "FizzBuzz"
+  if (int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
   elsif int % 3 == 0 
     puts "Fizz"
   elsif int % 5 == 0 
